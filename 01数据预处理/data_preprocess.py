5b526e3a757bb2ee3be07bc54c933e6574dbfa26
Zhang Shiwei <zhangshiwei17173@outlook.com>
2019-12-10 15:11:02 +0800

    update

--- 01数据预处理/data_preprocess.py
+++ 01数据预处理/data_preprocess.py
@@ -4,15 +4,15 @@
 import re
 
 # 标点符号和特殊字符
-punctuation = '''，。、:；（）ＸX×xa"“”,<《》.\t\n'''
+punctuation = '''，。、:；（）?ＸX×xa"“”,<《》.\t\n'''
 # 冗余词
 re_extra = re.compile(
-    r'(缓刑考验|刑期).*?(止|计算)|判决执行.*?止|(依照|依据).*?规定|《.*?》|第.*?条|第.*?款|的|了|其|另|已|且')
+    r'(缓刑考验|刑期).*?(止|计算)|判决执行.*?止|(依照|依据).*?规定|(扣押)?作案工具.*?没收|《.*?》|如不服.*份|第.*?条|第.*?款|的|了|其|另|已|且|审判.*?月')
 
 
 def preprocess(line):
     '''数据预处理'''
     line1 = re.sub(u"（.*?）", "", line)  # 去除括号内注释
     line2 = re.sub("[%s]+" % punctuation, "", line1)  # 去除标点、特殊字符
-    line3 = re_extra.sub("", line2)  # 去除冗余词
-    return line3
+    # line3 = re_extra.sub("", line2)  # 去除冗余词
+    return line2
--- 01数据预处理/excel2txt.py
+++ 01数据预处理/excel2txt.py
@@ -1,4 +1,5 @@
 # 从excel中导出txt格式的案件信息
+# 分别为辩护人意见、审理查明、法院意见和判决结果
 
 import xlrd
 import re
@@ -13,17 +14,18 @@ records = sheet0.col_values(21)[1:]  # 第21列代表庭审过程
 court_opinion = sheet0.col_values(27)[1:]  # 第27列代表法院意见
 sentence = sheet0.col_values(29)[1:]  # 第29列代表判决结果
 
+re_argument = re.compile(r"辩护人.*?(提出|认为|所提|辩解|要求|建议|辩护|辩称).*?。")
+re_truth = re.compile(r"(公诉机关指控|检察院.{0,5}指控|经.*?查明).*?(上述事实|以上事实|公诉机关认为|公诉机关认定|为证实|该院认为|检察院认为|被告人.*?异议)")
+re_sentence = re.compile(r"判决如下.*?(被告人.*(年|月|处罚))")
+
 # 写入文本文件
 f1 = open("data/argument.txt", "w", encoding='utf-8')   # 辩护人意见
 f2 = open("data/truth.txt", 'w', encoding='utf-8')  # 审理查明
 f3 = open("data/court_opinion.txt", 'w', encoding='utf-8')  # 法院意见
 f4 = open("data/sentence.txt", 'w', encoding='utf-8')  # 判决结果
 
-re_argument = re.compile(r"辩护人.*?(提出|认为|所提|辩解|要求|建议|辩护|辩称).*?。")
-re_truth = re.compile(r"(公诉机关指控|检察院指控|审理查明).*?上述事实")
-
-
 for elem1, elem2, elem3 in list(zip(records, court_opinion, sentence)):
+    # 预处理
     elem1 = preprocess(elem1)
     elem2 = preprocess(elem2)
     elem3 = preprocess(elem3)
@@ -33,7 +35,6 @@ for elem1, elem2, elem3 in list(zip(records, court_opinion, sentence)):
         f1.write(search_argument.group() + '\n')
     else:
         f1.write("None\n")
-
     # 写入审理查明
     search_truth = re_truth.search(elem1)
     if search_truth:
@@ -43,7 +44,11 @@ for elem1, elem2, elem3 in list(zip(records, court_opinion, sentence)):
     # 写入法院意见
     f3.write(elem2 + '\n')
     # 写入判决结果
-    f4.write(elem3+'\n')
+    search_sentence = re_sentence.search(elem3)
+    if search_sentence:
+        f4.write(search_sentence.group(1) + '\n')
+    else:
+        f4.write("None\n")
 
 
 f1.close()
