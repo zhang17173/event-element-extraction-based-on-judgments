0b77cdd500910ea96c1df01b4738d33a3ed46bd6
Zhang Shiwei <zhangshiwei17173@outlook.com>
2019-12-12 14:52:16 +0800

    Update

--- 01数据预处理/excel2txt.py
+++ 01数据预处理/excel2txt.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # 从excel中导出txt格式的案件信息
 # 分别为辩护人意见、审理查明、法院意见和判决结果
 
@@ -6,7 +8,7 @@ import re
 from data_preprocess import preprocess
 
 
-excel_file = "data/案件.xlsx"  # excel文件路径
+excel_file = "../data/案件.xlsx"  # excel文件路径
 wb = xlrd.open_workbook(excel_file)
 sheet0 = wb.sheets()[0]  # 获取第一个工作表
 
@@ -14,16 +16,17 @@ records = sheet0.col_values(21)[1:]  # 第21列代表庭审过程
 court_opinion = sheet0.col_values(27)[1:]  # 第27列代表法院意见
 sentence = sheet0.col_values(29)[1:]  # 第29列代表判决结果
 
+
 re_argument = re.compile(r"辩护人.*?(提出|认为|所提|辩解|要求|建议|辩护|辩称).*?。")
 re_truth = re.compile(r"(公诉机关指控|检察院.{0,5}指控|经.*?查明).*?(上述事实|以上事实|上述案件事实|公诉机关认为|公诉机关认定|为证实|该院认为|检察院认为|被告人.*?异议)")
 re_truth_1 = re.compile(r"(公诉机关指控|检察院.{0,5}指控|经.*?查明).*")
 re_sentence = re.compile(r"判决如下.*?(被告人.*(年|月|处罚))")
 
 # 写入文本文件
-f1 = open("data/argument.txt", "w", encoding='utf-8')   # 辩护人意见
-f2 = open("data/truth.txt", 'w', encoding='utf-8')  # 审理查明
-f3 = open("data/court_opinion.txt", 'w', encoding='utf-8')  # 法院意见
-f4 = open("data/sentence.txt", 'w', encoding='utf-8')  # 判决结果
+f1 = open("../data/argument.txt", "w", encoding='utf-8')   # 辩护人意见
+f2 = open("../data/truth.txt", 'w', encoding='utf-8')  # 审理查明
+f3 = open("../data/court_opinion.txt", 'w', encoding='utf-8')  # 法院意见
+f4 = open("../data/sentence.txt", 'w', encoding='utf-8')  # 判决结果
 
 for elem1, elem2, elem3 in list(zip(records, court_opinion, sentence)):
     # 预处理
