267a4c98095c086051c820abfc5e6bc17c8bda48
zhang_wsl <2732338615@qq.com>
2019-12-19 17:14:06 +0800

    update

--- 01数据预处理/data_preprocess.py
+++ 01数据预处理/data_preprocess.py
@@ -8,13 +8,13 @@ punctuation = '''，。、:；（）?ＸX×xa"“”,<《》.\t\n'''
 # 冗余词
 re_extra1 = re.compile(
     r'(缓刑考验|刑期).*?(止|计算)|判决执行.*?止|(依照|依据).*?规定|(扣押)?作案工具.*?没收|《.*?》|如不服.*份|第.*?条|第.*?款|的|了|其|另|已|且|审判.*?月')
-re_extra = re.compile(r'的|了|其|已|且')
+re_extra2 = re.compile(r'的|了|已|且')
 
 
 def preprocess(line):
     '''数据预处理'''
     line1 = re.sub(u"（.*?）", "", line)  # 去除括号内注释
     line2 = re.sub("[%s]+" % punctuation, "", line1)  # 去除标点、特殊字符
-    line3 = re_extra.sub("", line2)  # 去除冗余词
+    line3 = re_extra2.sub("", line2)  # 去除冗余词
     return line3
 
--- 02分词、词性标注、命名实体识别/seg_pos_ner.py
+++ 02分词、词性标注、命名实体识别/seg_pos_ner.py
@@ -3,46 +3,50 @@ from pyltp import Postagger
 from pyltp import NamedEntityRecognizer
 
 # ltp模型目录的路径，根据实际情况修改
-LTP_DATA_DIR = '/home/zhangshiwei/ltp_data_v3.4.0/'
+LTP_DATA_DIR = '/home/zhang17173/ltp_data_v3.4.0'
 # 词性标注模型路径，模型名称为`pos.model`
 pos_model_path = os.path.join(LTP_DATA_DIR, 'pos.model')
 # 命名实体识别模型路径，模型名称为`ner.model`
 ner_model_path = os.path.join(LTP_DATA_DIR, 'ner.model')
 
 
-# 分词
-cws_path = "/home/zhangshiwei/下载/ltp-3.4.0/bin/examples"  # 填写ltp可执行文件路径
-input_path = "/home/zhangshiwei/event-element-extraction-based-on-judgments/data/truth.txt"  # 输入文件路径
-temp_path = "/home/zhangshiwei/event-element-extraction-based-on-judgments/data/temp.txt"  # 临时文件路径
+# 使用编译好的LTP可执行程序进行分词，而不是直接调用pyltp的接口
+cws_path = "/home/zhang17173/ltp-3.4.0/bin/examples"  # 填写ltp可执行文件目录路径
+input_path = "data/original_data/court_opinion.txt"  # 输入文件路径
+temp_path = "/home/zhang17173/event-element-extraction-based-on-judgments/02分词、词性标注、命名实体识别/temp.txt"  # 临时文件路径
+
 # 词性标注
 postagger = Postagger()  # 初始化词性标注实例
 postagger.load(pos_model_path)  # 加载模型
+
 # 命名实体识别
 recognizer = NamedEntityRecognizer()  # 初始化命名实体识别实例
 recognizer.load(ner_model_path)  # 加载模型
 
-# 按行读取案件
+# 按行读取案件，进行分词、词性标注、命名实体识别等处理
 f1 = open(input_path, "r", encoding="utf-8")
-f2 = open("data/seg_pos_ner_result.txt", "w", encoding="utf-8")
+f2 = open("02分词、词性标注、命名实体识别/seg_pos_ner_result.txt", "w", encoding="utf-8")
 lines = f1.readlines()
-for line in lines:
-    f3 = open("data/temp.txt", "w", encoding="utf-8")
+for line in lines[:10]:
+    # 创建临时文件用以分词
+    f3 = open(temp_path, "w", encoding="utf-8")
     f3.write(line)
     f3.close()
     command1 = "cd " + cws_path
     command2 = "./cws_cmdline --threads 24 --input " + \
         temp_path + " --segmentor-lexicon dict"
-    command = command1 + "&&" + command2
-    f = os.popen(command)
+    f = os.popen(command1 + "&&" + command2)
     words = f.read().strip("\n").split("\t")
+    print(words)
     postags = postagger.postag(words)  # 词性标注
     netags = recognizer.recognize(words, postags)  # 命名实体识别
 
+    # 写入结果
     for word, postag, netag in zip(words, postags, netags):
+        print(word, postag, netag)
         f2.write(word + "\t" + postag + "\t" + netag + "\n")
 
     f2.write("\n")
-
 
 # 释放模型
 postagger.release()
@@ -50,3 +54,4 @@ recognizer.release()
 
 f1.close()
 f2.close()
+
