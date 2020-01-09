b028e0b3c5e80a1fda4228b5ddcc6555370fe904
YiXiangRong <921118570@qq.com>
2020-01-08 23:51:37 -0500

    update extract_elements

--- 03事件要素提取/extract_elements.py
+++ 03事件要素提取/extract_elements.py
@@ -1,4 +1,6 @@
 import sys
+import csv
+import glob
 sys.path.append("/home/yxr/event-element-extraction-based-on-judgments/04判决结果预测")
 from utils import *
 from rules import *
@@ -9,6 +11,8 @@ sentence = "/home/yxr/event-element-extraction-based-on-judgments/data/original_
 truth = "/home/yxr/event-element-extraction-based-on-judgments/data/original_data/truth.txt"
 crf_result = "/home/yxr/event-element-extraction-based-on-judgments/data/crf_result/单个案件/"
 
+
+
 f1 = open(court_opinion, "r", encoding="utf-8")
 f2 = open(sentence, "r", encoding="utf-8")
 f3 = open(truth, "r", encoding="utf-8")
@@ -16,13 +20,35 @@ lines1 = f1.readlines()
 lines2 = f2.readlines()
 lines3 = f3.readlines()
 elements = dict() #用来存储所有要素
+patterns = dict() #用来存储所有特征
+headers = [
+    '01死亡人数',
+    "02轻微伤人数",
+    "03轻伤人数",
+    "04重伤人数",
+    "05罪名",
+    "06犯罪阶段",
+    "07是否投案",
+    "08是否如实供述",
+    "09是否自首",
+    "10是否认罪",
+    "11是否赔偿",
+    "12是否取得谅解",
+    "13是否立功",
+    "14是否限制刑事责任能力",
+    "15是否有前科",
+    "16被害人是否有过错",
+    "判决结果"
+]
+rows = []
 for idx, (line1, line2, line3) in enumerate(zip(lines1, lines2, lines3)):
     elements["罪名"] = crime_name(line2)
     elements["犯罪阶段"] = crime_stage(line1)
     elements["是否限制刑事责任能力"] = limited_responsibility(line1)
     elements["是否有前科"] = pedigree(line1)
+    elements["是否立功"] = contribute(line1)
     elements["被害人是否有过错"] = victim_fault(line1)
-    elements["受伤情况"] = damage(line3)
+    elements["受伤情况"] = damage(line1)
     elements["死亡情况"] = death(line1)
     elements["判决结果"] = sentence_result(line2)
 
@@ -37,5 +63,14 @@ for idx, (line1, line2, line3) in enumerate(zip(lines1, lines2, lines3)):
     """
     positives = get_event_elements(crf_result+str(idx+1)+".txt")
     elements.update(positives)
-    #print(cn, cs, lr, pd, vf, dm, sr, positives, sep="\n")
-    print(elements)
+    patterns = get_patterns(elements)
+    print(patterns)
+    rows.append(patterns)
+
+# 写回数据
+with open("/home/yxr/event-element-extraction-based-on-judgments/data/data_16.csv", "w", newline='') as f:
+    f_csv = csv.DictWriter(f, headers)
+    # f_csv.writeheader()
+    f_csv.writerows(rows)
+
+
